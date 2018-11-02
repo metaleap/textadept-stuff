53258ac76c885f5e97be9a5d2966d8914c85770f
metaleap <metaleap@users.noreply.github.com>
2018-11-02 23:39:47 +0100

    [autopush] «And that's the way it is... -- Walter Cronkite»

---
+++ favcmds.lua
@@ -0,0 +1,59 @@
+local me = {}
+
+local function fillInCmd(cmd)
+    local pat = "%$%(([%w_]+)%)"
+    local names = {}
+
+    cmd:gsub(pat, function(name)
+        names[1 + #names] = name
+    end)
+
+    if #names > 0 then
+        local inputbox = { title = cmd, informative_text = { cmd }, text = {}, width = 2345, height = 1234 }
+        for _, name in ipairs(names) do
+            inputbox.informative_text[1 + #inputbox.informative_text] = name
+            inputbox.text[1 + #inputbox.text] = ''
+        end
+        local button, inputs = ui.dialogs.standard_inputbox(inputbox)
+        if button == 1 then
+            local kvs = {}
+            if type(inputs) == 'table' then
+                for i, name in ipairs(names) do
+                    kvs[name] = inputs[i]
+                end
+            else
+                kvs[names[1]] = inputs
+            end
+            cmd = cmd:gsub(pat, function(name)
+                return kvs[name]
+            end)
+        else
+            cmd = ""
+        end
+    end
+
+    return cmd
+end
+
+local function onCmd(cmd)
+    return function()
+        cmd = fillInCmd(cmd)
+        if #cmd > 0 then
+            f = io.popen(cmd, 'r')
+            for ln in f:lines() do
+                ui.print(ln)
+            end
+            f:close()
+        end
+    end
+end
+
+function me.init(favCmds)
+    local menu = { title = ''}
+    for _, favcmd in ipairs(favCmds) do
+        menu[1 + #menu] = { favcmd, onCmd(favcmd) }
+    end
+    textadept.menu.menubar[1 + #textadept.menu.menubar] = menu
+end
+
+return me
--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
 local me = {}
 
 local favdirs = require 'metaleap_zentient.favdirs'
+local favcmds = require 'metaleap_zentient.favcmds'
 
 keys['f1'] = {}
 me.langProgs = {}
+me.favCmds = {}
 me.favDirs = {}
 
 function me.startUp()
     favdirs.init(me.favDirs)
+    favcmds.init(me.favCmds)
 end
 
 return me
