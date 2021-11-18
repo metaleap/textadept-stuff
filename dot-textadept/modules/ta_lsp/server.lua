2ddbeb9ac934ff65e93a01c47ba7481ca0e1ef6f
metaleap <metaleap@users.noreply.github.com>
2021-11-18 23:21:16 +0100

    [autopush] «Eat right, stay fit, and die anyway.»

--- dot-textadept/modules/ta_lsp/server.lua
+++ dot-textadept/modules/ta_lsp/server.lua
@@ -1,6 +1,6 @@
 local json = require('ta_lsp.dkjson')
 
-local Server = {}
+local Server = { log_rpc = true, allow_markdown_docs = true }
 
 function Server.new(lang, desc)
     local me = {lang = lang, desc = desc, _reqid = 0}
@@ -27,7 +27,7 @@ end
 function Server.ensureProc(me)
     local err
     if not Server.chk(me) then
-        me._reqid = 0
+        me._reqid, me._initRecv = 0, false
         me.proc, err = os.spawn(me.desc.cmd, me.desc.cwd or lfs.currentdir(),
                                 Server.onStdout(me), Server.onStderr(me), Server.onExit(me))
         if err then
@@ -36,6 +36,17 @@ function Server.ensureProc(me)
         end
         if me.proc then
             Server.log(me, me.proc:status())
+            Server.sendRequest(me, 'initialize', {
+                processId = json.null, rootUri = json.null,
+                initializationOptions = me.desc.init_options or json.null,
+                capabilities = {
+                    window = {showMessage = {}},
+                    workspace = {},
+                    textDocument = {
+                        hover = { contentFormat = Server.allow_markdown_docs and { 'markdown', 'plaintext' } or { 'plaintext' } }
+                    }
+                }
+            })
         end
     end
     return Server.chk(me)
@@ -62,25 +73,43 @@ function Server.onStdout(me) return function(data)
     Server.onIncomingRaw(me, data)
 end end
 
-function Server.onIncomingRaw(me, data)
-    Server.log(me, data)
-end
-
-function Server.sendRaw(me, data)
+function Server.sendMsg(me, msg, addreqid)
     if Server.ensureProc(me) then
-        local ok, err = me.proc:write(data)
+        if addreqid then
+            me._reqid = me._reqid + 1
+            msg.id = me._reqid
+        end
+        local data = json.encode(msg)
+        if Server.log_rpc then
+            Server.log(me, data)
+        end
+        local ok, err = me.proc:write("Content-Length: "..(#data+2).."\r\n\r\n"..data.."\r\n")
         if (not ok) and err and string.len(err) > 0 then
             Server.die(me)
             Server.log(me, err)
             -- the below, via ensureProc, will restart server, which sends init stuff before ours. (they do crash sometimes, or pipes break..)
-            Server.sendRaw(me, data)
+            return Server.sendMsg(me, data)
         end
     end
+    return msg.id
+end
+
+function Server.sendNotify(me, method, params)
+    Server.sendMsg(me, {jsonrpc = '2.0', method = method, params = params})
+end
+
+function Server.sendResponse(me, reqid, result)
+    Server.sendMsg(me, {jsonrpc = '2.0', id = reqid, result = result})
 end
 
 function Server.sendRequest(me, method, params)
-    me._reqid = me._reqid + 1
+    return Server.sendMsg(me, {jsonrpc = '2.0', method = method, params = params}, true)
 end
+
+function Server.onIncomingRaw(me, data)
+    Server.log(me, data)
+end
+
 
 
 
