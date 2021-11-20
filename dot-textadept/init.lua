c2aa36e07dc078ce916ff61ec7c7d447f345e6b1
metaleap <metaleap@users.noreply.github.com>
2021-11-20 16:38:01 +0100

    [autopush] «Live Free or Live in Massachusettes.»

--- dot-textadept/init.lua
+++ dot-textadept/init.lua
@@ -250,8 +250,8 @@ end
 
 -- lang-specific stuff
 textadept.file_types.extensions.dummy = 'dummy'
---textadept.file_types.extensions.go = 'gopls'
-lsp_adept.lang_servers.dummy = {cmd = 'dummylangserver', init_options = nil}
+--lsp_adept.lang_servers.dummy = {cmd = 'dummylangserver'}
+lsp_adept.lang_servers.go = {cmd = 'pipethru'}
 local onBuildOrRun = function(str)
     clearDbgBufs()
     ui.print("")
