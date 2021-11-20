4cb27231999a46d29007cd42b748c4720affd15d
metaleap <metaleap@users.noreply.github.com>
2021-11-20 03:10:43 +0100

    modules not needed in git

--- dot-textadept/modules/lsp-adept/common.lua
+++
@@ -1,9 +0,0 @@
-local json = require('lsp-adept.deps.dkjson')
-
-local Common = {
-    LspAdept = nil, -- ./init.lua sets this
-    json_empty = json.decode('{}') -- plain lua {}s would mal-encode into json []s
-}
-
-
-return Common
